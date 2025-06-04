ec8c088f9827239126abec9022a6b981c8428eb6
StefanoLorato <stefanolorato97@gmail.com>
2025-06-04 09:24:47 +0200

    Initial commit

---
+++ src/main/java/org/generation/italy/collectionarchive/CollectionarchiveApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy.collectionarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionarchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionarchiveApplication.class, args);
+	}
+
+}
