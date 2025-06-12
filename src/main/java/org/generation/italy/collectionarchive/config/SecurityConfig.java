01cfc7e7d375b6a47add1c78ddd20deadd7ec8d4
martapetruzzelli <martapetruzzelli1@gmail.com>
2025-06-12 10:39:14 +0200

    aggiunge a lista bianca le get per collections e items

--- src/main/java/org/generation/italy/collectionarchive/config/SecurityConfig.java
+++ src/main/java/org/generation/italy/collectionarchive/config/SecurityConfig.java
@@ -61,7 +61,6 @@ public class SecurityConfig {
         };
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults())
